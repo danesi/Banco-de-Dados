4bfed2ae35f92b0868fce037131784b023f7ed2e
Daniel Anesi <daniel.o.anesi@gmail.com>
2018-04-16 08:05:51 -0300

    Exercicos 3 semestre

---
+++ Aeroporto.sql
@@ -0,0 +1,423 @@
+﻿create table companhia (
+codc 	integer not null,
+nomec 	varchar(30) not null,
+pais 	varchar(15),
+primary key (codc));
+
+create table piloto (
+codp 	integer not null,
+nomep 	varchar(30) not null,
+salario 	real not null,
+gratificacoes 	real,
+anoingresso 	integer,
+pais 	varchar(15),
+codc 	integer not null,
+primary key (codp),
+foreign key (codc) references companhia);
+
+create table aeroporto (
+coda 	varchar(3) not null,
+nomea 	varchar(30) not null,
+cidade 	varchar(20),
+pais 	varchar(15),
+primary key (coda));
+
+create table voo (
+codv 	varchar(6) not null,
+origem 	varchar(3) not null,
+dest 	varchar(3) not null,
+hora 	time,
+primary key (codv),
+foreign key (origem) references aeroporto,
+foreign key (dest) references aeroporto);
+
+create table escala (
+codv 	varchar(6) not null,
+data 	date not null,
+codp 	integer not null,
+aviao 	varchar(15),
+primary key (codv, data),
+foreign key (codv) references voo,
+foreign key (codp) references piloto);
+
+
+insert into companhia values (1, 'TAM', 'Brasil');
+insert into companhia values (2, 'Varig', 'Brasil');
+insert into companhia values (3, 'GOL', 'Brasil');
+insert into companhia values (4, 'American Airlines', 'Estados Unidos');
+insert into companhia values (5, 'Air France', 'França');
+insert into companhia values (6, 'Lufthansa', 'Alemanha');
+insert into companhia values (7, 'TAP', 'Portugal');
+
+insert into piloto values (1, 'Pedro', 12000, 2000, 1990, 'Brasil', 2);
+insert into piloto values (2, 'Franz', 15000, 1000, 2000, 'Alemanha', 6);
+insert into piloto values (3, 'Alex', 8000, NULL, 2007, 'Brasil', 3);
+insert into piloto values (4, 'Jean', 12450, 500, 2005, 'França', 5);
+insert into piloto values (5, 'Eric', 9000, NULL, 2005, 'Brasil', 3);
+insert into piloto values (6, 'Daniel', 7000, NULL, 2008, 'Brasil', 2);
+insert into piloto values (7, 'Manuel', 13250, 1500, 1995, 'Portugal', 7);
+insert into piloto values (8, 'Tom', 18000, 3000, 1995, 'Estados Unidos', 4);
+insert into piloto values (9, 'Osama', 11000, 1200, 2001, 'Iraque', 1);
+insert into piloto values (10, 'Yuri', 10000, 950, 2002, 'Rússia', 5);
+
+insert into aeroporto values ('TXL', 'Tegel', 'Berlim', 'Alemanha');
+insert into aeroporto values ('EZE', 'Ezeiza', 'Buenos Aires', 'Argentina');
+insert into aeroporto values ('JFK', 'Kennedy International', 'Nova Iorque', 'Estados Unidos');
+insert into aeroporto values ('FCO', 'Leonardo da Vinci', 'Roma', 'Itália');
+insert into aeroporto values ('MVD', 'Aeroporto Carrasco', 'Montevidéo', 'Uruguai');
+insert into aeroporto values ('POA', 'Salgado Filho', 'Porto Alegre', 'Brasil');
+insert into aeroporto values ('GIG', 'Tom Jobim', 'Rio de Janeiro', 'Brasil');
+insert into aeroporto values ('GRU', 'Guarulhos', 'São Paulo', 'Brasil');
+insert into aeroporto values ('BSB', 'Brasília', 'Brasília', 'Brasil');
+insert into aeroporto values ('SDU', 'Santos Dumont', 'Rio de Janeiro', 'Brasil');
+insert into aeroporto values ('CGH', 'Congonhas', 'São Paulo', 'Brasil');
+
+insert into voo values ('VG001', 'BSB', 'POA', '13:00');
+insert into voo values ('TM123', 'GRU', 'EZE', '08:00');
+insert into voo values ('GL456', 'SDU', 'CGH', '15:00');
+insert into voo values ('AF441', 'GIG', 'FCO', '22:00');
+insert into voo values ('TP678', 'FCO', 'GRU', '06:00');
+insert into voo values ('LF098', 'TXL', 'EZE', '06:00');
+insert into voo values ('AA765', 'MVD', 'JFK', '17:00');
+insert into voo values ('TM432', 'CGH', 'GIG', '02:00');
+insert into voo values ('GL111', 'EZE', 'POA', '12:00');
+insert into voo values ('VG230', 'EZE', 'POA', '08:00');
+insert into voo values ('VG333', 'POA', 'MVD', '10:00');
+insert into voo values ('AF777', 'JFK', 'FCO', '09:00');
+insert into voo values ('TM999', 'GRU', 'TXL', '20:00');
+
+insert into escala values ('VG001', '2008-04-13', 1, 'Boeing 737');
+insert into escala values ('TM123', '2008-04-14', 9, 'Airbus A320');
+insert into escala values ('GL456', '2008-04-13', 3, 'Boeing 737');
+insert into escala values ('AF441', '2008-04-15', 4, 'Boeing 747');
+insert into escala values ('TP678', '2008-04-15', 7, 'Airbus A330');
+insert into escala values ('LF098', '2008-04-14', 2, 'Airbus A330');
+insert into escala values ('AA765', '2008-04-16', 8, 'MD11');
+insert into escala values ('TM432', '2008-04-16', 9, 'Fokker 100');
+insert into escala values ('GL111', '2008-04-15', 3, 'Boeing 737');
+insert into escala values ('VG230', '2008-04-15', 1, 'MD11');
+insert into escala values ('VG333', '2008-04-13', 6, 'MD11');
+insert into escala values ('AF777', '2008-04-13', 10, 'Boeing 747');
+insert into escala values ('TM999', '2008-04-17', 9, 'Airbus A320');
+insert into escala values ('VG001', '2008-04-20', 6, 'MD11');
+insert into escala values ('TM123', '2008-04-19', 9, 'Boeing 737');
+insert into escala values ('GL456', '2008-04-18', 5, 'Boeing 737');
+insert into escala values ('AF441', '2008-04-20', 10, 'Airbus A330');
+insert into escala values ('TP678', '2008-04-18', 7, 'Boeing 747');
+insert into escala values ('LF098', '2008-04-17', 2, 'Boeing 747');
+insert into escala values ('AA765', '2008-04-20', 8, 'Boeing 787');
+insert into escala values ('TM432', '2008-04-17', 9, 'Fokker 100');
+insert into escala values ('GL111', '2008-04-18', 5, 'Fokker 100');
+insert into escala values ('VG230', '2008-04-19', 6, 'Boeing 707');
+insert into escala values ('VG333', '2008-04-19', 6, 'Boeing 737');
+insert into escala values ('AF777', '2008-04-16', 4, 'Airbus A340');
+insert into escala values ('TM999', '2008-04-16', 9, 'Airbus A320');
+
+======================================================================================================
+select * from companhia;
+select * from piloto;
+select * from aeroporto;
+select * from voo;
+select * from escala;
+
+--7. Caso os valores da coluna GRATIFICACOES da tabela PILOTO tenham sido inseridos com NULL,
+--escreva um comando para atualizar (UPDATE) tais valores para 0 (zero).
+update piloto
+set gratificacoes = 0
+where gratificacoes is null;
+
+--a) Os dados de todos os pilotos de companhias brasileiras.
+SELECT * 
+FROM piloto
+WHERE piloto.codc in
+	(select companhia.codc 
+	from companhia
+	where companhia.pais = 'Brasil');
+
+--b) O nome de todos os pilotos da Varig.
+SELECT nomep
+FROM piloto
+WHERE piloto.codc in
+	(select companhia.codc 
+	from companhia
+	where companhia.nomec = 'Varig');
+
+--c) O nome de todos os pilotos, junto com seu salário e gratificação.
+select nomep, salario, gratificacoes
+from piloto;
+
+--d) O nome de todos os pilotos, junto com o total de seus vencimentos, para todos os pilotos que
+--voam há pelo menos 5 anos.
+select nomep,(salario + gratificacoes) AS "Vencimentos"
+from piloto
+where anoingresso < 2003;
+
+--e) O nome da cidade e país de destino do voo VG230.
+select aeroporto.cidade, aeroporto.pais
+from voo, aeroporto
+where voo.dest = aeroporto.coda and codv = 'VG230';
+
+--f) As companhias dos vôos escalados para 13/04/2008.
+select nomec
+from companhia
+where codc in
+	(select codc
+	from piloto
+	where codp in
+		(select codp
+		from escala
+		where data = '2008/04/13'));
+		
+
+--g) O código de todos os vôos que iniciam por VG ou TM, junto com a respectiva companhia.
+(select v.codv, c.nomec 
+from companhia c, piloto p, escala e, voo v
+where c.codc = p.codc and p.codp = e.codp and e.codv = v.codv and v.codv like 'VG%')
+union
+(select v.codv, c.nomec 
+from companhia c, piloto p, escala e, voo v
+where c.codc = p.codc and p.codp = e.codp and e.codv = v.codv and v.codv like 'TM%');
+
+--h) O código de todos os vôos, nome dos pilotos escalados para os mesmos, e respectivos tipo de
+--avião e companhia.
+select distinct v.codv, p.nomep, c.nomec, e.aviao
+from companhia c, piloto p, escala e, voo v
+where c.codc = p.codc and p.codp = e.codp and e.codv = v.codv;
+
+
+--i) O código de todos os vôos e hora de saída, junto com o nome dos pilotos escalados para os
+--mesmos, e respectivos tipo de avião e companhia, para todos os vôos de companhias
+--estrangeiras.
+select v.codv, p.nomep, c.nomec, e.aviao
+from companhia c, piloto p, escala e, voo v
+where c.codc = p.codc and p.codp = e.codp and e.codv = v.codv and c.pais <> 'Brasil';
+
+
+--j) O código de todos os vôos para a Alemanha ou Itália, com as respectivas data e hora de saída.
+(select distinct v.codv, e.data, v.hora
+from escala e, aeroporto a, voo v
+where a.coda = v.origem and v.codv = e.codv and a.pais = 'Alemanha')
+union
+(select distinct v.codv, e.data, v.hora
+from escala e, aeroporto a, voo v
+where a.coda = v.dest and v.codv = e.codv and a.pais = 'Itália');
+
+--k) A companhia dos pilotos que voam para o Brasil.
+select distinct c.nomec
+from piloto p, companhia c
+where p.codc = c.codc and p.codp in
+	(select codp
+	from escala
+	where codv in
+		(select codv
+		from voo
+		where dest in
+			(select coda
+			from aeroporto
+			where pais = 'Brasil')));
+
+--l) O destino (nome do aeroporto, cidade e país) de todos os vôos da Varig.
+select nomea, cidade, pais
+from aeroporto
+where coda in
+	(select dest
+	from voo
+	where codv in
+		(select codv
+		from escala
+		where codp in
+			(select codp
+			from piloto
+			where codc in
+				(select codc
+				from companhia 
+				where nomec = 'Varig'))));
+
+--m) O nome dos aeroportos de origem e de destino de todos os vôos marcados para o dia
+--15/04/2008.
+select a.nomea as "Origem", b.nomea as "Destino"
+from voo v, aeroporto a, escala e, aeroporto b
+where v.origem = a.coda and v.dest = b.coda and v.codv = e.codv and e.data = '2008/04/15';
+
+--n) Os aeroportos de destino (nome, cidade) para todos os vôos da Varig em MD11.
+select nomea, cidade
+from aeroporto
+where coda in
+	(select dest
+	from voo
+	where codv in
+	        (select codv
+		from escala
+		where aviao = 'MD11' and codp in
+			(select codp
+			from piloto
+			where codc in
+				(select codc
+				from companhia
+				where nomec = 'Varig'))));
+
+
+
+
+
+
+
+
+
+--o) O código e horario dos vôos internos de todos os países.
+duplicar o aeroporto
+
+--p) O código de todos os vôos internacionais da varig.
+--Dica: Todas as companhias só podem operar a partir de seu próprio país, ou retornar a este.
+
+
+--q) O nome de todos os aeroportos onde a Varig opera.
+
+
+--r) O código de todos os vôos internacionais que as companhias fazem a partir de aeroportos em
+--seus próprios países.
+
+
+--s) O nome dos pilotos que voam de MD11 para o seu próprio país.
+
+
+
+********************************************************************************************
+********************************************************************************************
+********************************************************************************************
+
+Desenvolva as seguintes consultas em SQL, mostrando o resultado final, de acordo com as
+informações inseridas (mostrar a consulta também):
+
+select * from companhia;
+select * from piloto;
+select * from aeroporto;
+select * from voo;
+select * from escala;
+
+--1. O salário do piloto mais bem pago da Varig.
+select max(salario)
+from piloto
+where codc in
+	(select codc
+	from companhia
+	where nomec = 'Varig');
+--2. O menor, maior, e média dos salários dos pilotos de companhias brasileiras.
+select max(salario), min(salario), avg(salario)
+from piloto
+where codc in
+	(select codc
+	from companhia
+	where pais = 'Brasil');
+--3. O total da folha de pagamento da Varig.
+select sum(salario + gratificacoes) as "TotalPag"
+from piloto
+where codc in
+	(select codc
+	from companhia
+	where nomec = 'Varig');
+--4. O número de escalas previstas para o vôo VG230.
+select count(codv) as "Numeros de escalas"
+from escala
+where codv in
+	(select codv
+	from voo
+	where codv = 'VG230');
+--5. O número de pessoas envolvidas em escalas do vôo VG230.
+select count(codp)
+from piloto
+where codp in
+	(select codp
+	from escala
+	where codv in
+		(select codv
+		from voo
+		where codv = 'VG230'));
+--6. O número de aeroportos no Brasil.
+select count (coda)
+from aeroporto
+where  pais = 'Brasil';
+--7. O número de países e de cidades diferentes para as quais se destinam os vôos partindo de GRU.
+select count(ao.coda) as "Países", count(a.coda) as "Cidades"
+from aeroporto ao, aeroporto a, voo v
+where v.origem = ao.coda and v.dest = a.coda and v.origem = 'GRU';
+	
+--8. O número de aeroportos diferentes de onde os pilotos da Varig partem.
+select count(coda)
+from aeroporto
+where coda in
+	(select origem
+	from voo
+	where codv in
+		(select codv
+		from escala
+		where codp in
+			(select codp
+			from piloto
+			where codc in
+				(select codc
+				from companhia
+				where nomec = 'Varig'))));
+--9. O número de diferentes vôos que a TAM oferece para São Paulo.
+select count(coda)
+from aeroporto
+where cidade = 'São Paulo' and coda in
+	(select dest
+	from voo
+	where codv in
+		(select codv
+		from escala
+		where codp in
+			(select codp
+			from piloto
+			where codc in 
+				(select codc
+				from companhia
+				where nomec = 'GOL'))));
+--10. O número de aeroportos diferentes que os pilotos da varig usam (para chegada ou partida).
+select count(distinct a.coda)
+from companhia c, piloto p, escala e, voo v, aeroporto a, aeroporto ae
+where c.codc = p.codc 
+and p.codp = e.codp 
+and e.codv = v.codv 
+and v.origem = a.coda
+and v.dest = ae.coda
+and c.nomec = 'Varig';
+--11. Para cada companhia, o menor, maior salário, bem como a média.
+select min(p.salario), max(p.salario), avg(p.salario)
+from companhia c, piloto p
+where c.codc = p.codc
+group by p.codc
+order by p.codc
+--12. Para cada cidade do Brasil, o número de aeroportos que possui.
+select cidade, count(coda) as "NumAeroportos"
+from aeroporto 
+where pais = 'Brasil'
+group by cidade
+--13. Para cada piloto, o número de escalas programadas para o dia 15/04/2008.
+select nomep, count(p.codp)
+from piloto p, escala e
+where p.codp = e.codp and data = '15/04/2008'
+group by p.codp 
+	
+--14. Para cada dia e hora, o número de vôos que partem pela manhã.
+select e.data, v.hora, count(e.codv)
+from escala e, voo v
+where v.hora < '12:00' and v.hora > '6:00' and e.codv = v.codv
+group by e.data, v.hora
+--15. Para cada piloto de companhia estrangeira, o número de vôos que ele faz de ou para o Brasil.
+
+--16. As companhias que pagam como salário mais alto a seus pilotos menos que 12000.
+
+--17. As companhias brasileiras que pagam como salário mais alto a seus pilotos menos que 12000.
+
+--18. As companhias estrangeiras que possuem pelo menos 2 pilotos.
+
+--19. O número de escalas previstas em MD11 para cada piloto da Varig que possui pelo menos 2
+--escalas neste tipo de avião.
+
+--20. As companhias que só usam um tipo de avião. 
+
+
+
+
---
+++ Futebol.sql
@@ -0,0 +1,192 @@
+﻿create table clube (
+codclube integer not null,
+nome char(40),
+dirigente char(40),
+primary key (codclube));
+
+create table jogador (
+codjogador integer not null,
+nome char(40),
+datanasc date,
+primary key (codjogador));
+
+
+create table patrocinador (
+codpatroc integer not null,
+nome char(40),
+primary key (codpatroc));
+
+create table joga (
+codclube integer not null,
+codjogador integer not null,
+datainicial date,
+datafinal date,
+posicao char(15),
+salario integer,
+primary key (codclube, codjogador),
+foreign key (codclube) references clube,
+foreign key (codjogador) references jogador);
+
+create table jogo (
+codclubecasa integer not null,
+codclubevisit integer not null,
+datajogo date,
+golsclubecasa integer,
+golsclubevisit integer,
+primary key (codclubecasa, codclubevisit, datajogo),
+foreign key (codclubecasa) references clube,
+foreign key (codclubevisit) references clube);
+
+
+create table patrocclube (
+codpatroc integer not null,
+codclube integer not null,
+primary key (codpatroc, codclube),
+foreign key (codpatroc) references patrocinador,
+foreign key (codclube) references clube);
+
+
+insert into clube values (01, 'Grêmio', 'Paulo Odone');
+insert into clube values (02, 'Internacional', 'Fernando Carvalho');
+insert into clube values (03, 'Corinthians', 'Roque Citadini');
+insert into clube values (04, 'Flamengo', 'Cléber Leite');
+insert into clube values (05, 'Palmeiras', 'Juvenal Juvêncio');
+
+insert into jogador values (01, 'Lucas', '12/02/1982');
+insert into jogador values (02, 'Galatto', '28/09/1984');
+insert into jogador values (03, 'Tinga', '21/10/1978');
+insert into jogador values (04, 'Clemer', '14/06/1972');
+insert into jogador values (05, 'Tevez', '02/04/1980');
+insert into jogador values (06, 'Marcos', '05/05/1976');
+insert into jogador values (07, 'Fabiano Eller', '07/12/1975');
+insert into jogador values (08, 'José', '08/04/1977');
+
+insert into patrocinador values (01, 'Tim');
+insert into patrocinador values (02, 'Banrisul');
+insert into patrocinador values (03, 'Panasonic');
+insert into patrocinador values (04, 'Siemens');
+insert into patrocinador values (05, 'Philips');
+
+insert into joga values (01, 01, '01/01/2005', null, 'Volante', 30000);
+insert into joga values (01, 02, '01/01/2005', null, 'Goleiro', 25000);
+insert into joga values (01, 03, '01/01/2001', '31/12/2003', 'Volante', 40000);
+insert into joga values (02, 03, '01/01/2004', null, 'Volante', 45000);
+insert into joga values (04, 04, '01/01/1998', '31/12/2001', 'Goleiro', 30500);
+insert into joga values (02, 04, '01/01/2002', null, 'Goleiro', 35000);
+insert into joga values (03, 05, '01/01/2004', null, 'Atacante', 100000);
+insert into joga values (01, 06, '01/01/1997', '31/12/1998', 'Goleiro', 50000);
+insert into joga values (05, 06, '01/01/1999', null, 'Goleiro', 70000);
+insert into joga values (02, 07, '01/01/2005', null, 'Zagueiro', 40000);
+insert into joga values (04, 07, '01/01/1999', '31/12/2003', 'Zagueiro', 30000);
+insert into joga values (03, 08, '01/01/1995', '31/12/1997', 'Atacante', 20000);
+insert into joga values (05, 08, '01/01/1998', '31/12/2000', 'Atacante', 30000);
+insert into joga values (01, 08, '01/01/2001', '31/12/2004', 'Atacante', 40000);
+insert into joga values (02, 08, '01/01/2006', null, 'Atacante', 50000);
+
+insert into jogo values (01, 02, '08/09/2000', 0, 0);
+insert into jogo values (01, 03, '04/02/2003', 2, 1);
+insert into jogo values (04, 02, '03/01/2002', 3, 2);
+insert into jogo values (05, 01, '02/12/2005', 1, 1);
+insert into jogo values (02, 01, '23/10/2001', 0, 1);
+insert into jogo values (03, 05, '21/08/2001', 3, 0);
+insert into jogo values (01, 04, '29/11/2004', 4, 1);
+insert into jogo values (03, 02, '11/04/2006', 1, 3);
+
+insert into patrocclube values (02, 01);
+insert into patrocclube values (02, 02);
+insert into patrocclube values (03, 02);
+insert into patrocclube values (05, 01);
+insert into patrocclube values (01, 04);
+insert into patrocclube values (04, 05);
+insert into patrocclube values (05, 03);
+insert into patrocclube values (01, 05);
+
+=========================================================================================================
+SELECT
+=========================================================================================================
+select * from clube;
+select * from jogador;
+select * from patrocinador;
+select * from joga;
+select * from jogo;
+select * from patrocclube;
+
+--1. Obter todas as informações de jogadores que tenham nascido após ‘01/01/1976’
+select * from jogador where datanasc > '01/01/1976';
+
+--2. Obter o nome de todos os patrocinadores que tenham seu nome iniciado com a letra ‘P’.
+select nome from patrocinador where nome like 'P%';
+
+--3. Obter o nome do jogador e sua posição, para todos os jogadores do clube 01. Ordenar por nome de jogador.
+select jogador.nome, joga.posicao from jogador, joga where jogador.codjogador = joga.codjogador and joga.codclube = 1 order by 1;
+
+--4. Obter o nome do clube e a posição jogada, para todos os clubes em que o jogador de nome ‘Marcos’ jogou. 
+--Ordenar o resultado por nome de clube. 
+select clube.nome, joga.posicao from clube, joga, jogador where clube.codclube = joga.codclube and joga.codjogador = jogador.codjogador and jogador.nome = 'Marcos';
+
+--5. Obter o nome do jogador, a data inicial e a posição, para todos os jogadores que jogam no clube
+--de nome ‘Internacional’ desde ‘01/01/2000’. Ordenar pelo nome do jogador. 
+select g.nome, j.datainicial, j.posicao from clube c, joga j, jogador g where c.codclube = j.codclube and j.codjogador = g.codjogador and c.nome = 'Internacional' and j.datainicial >= '01/01/2000';
+
+--6. Obter o nome do clube visitante, os gols do clube da casa, os gols do clube visitante e a data do
+--jogo, para todos os jogos do clube da casa de código 01, em que sua situação foi vitoriosa. 
+select clube.nome, jogo.golsclubecasa, jogo.golsclubevisit, jogo.datajogo from jogo, clube  where clube.codclube = 1 and jogo.golsclubecasa > jogo.golsclubevisit;
+
+--7. Obter o nome do clube e o número de patrocinadores por clube, para todos os clubes em que
+--jogou o jogador de nome ‘José’. Ordenar o resultado pelo nome do clube 
+
+--8. Obter o nome do clube, a quantidade de gols feitos e o número de jogos jogados, onde o clube
+--em questão é o de código 02 e sua situação nos jogos é a de visitante. 
+
+
+--9. Obter, para todos os jogadores, o nome do jogador, o número de clubes em que ele jogou e a
+--média dos salários ganhos nos clubes. 
+
+--10. Obter, para todos os jogos, o nome do clube da casa, o nome do clube visitante, a data do jogo,
+--o número de gols do clube da casa e o número de gols do clube visitante. Ordenar pela data dos jogos. 
+
+--11. faça uma consulta que retorne o nome do clube, bem como seus patrocinadores.
+select c.nome AS "Nome Clube", t.nome AS "Nome Patrocinador" from clube c, patrocclube p, patrocinador t where c.codclube = p.codclube and p.codpatroc = t.codpatroc;
+
+--12. faça uma consulta que retorne o nome dos clubes em que o jogador 'josé' jogou.
+select c.nome from jogador j, joga g, clube c where j.codjogador = g.codjogador and g.codclube = c.codclube and j.nome = 'José';
+
+================= SEM USAR PRODUTO CARTESIANO == ANINHADAS =========================================================
+select nome
+from clube
+where codclube in
+	(select codclube
+	from joga
+	where codjogador in
+		(select codjogador
+		 from jogador
+		 where nome = 'José'));
+=======================================================================================================================
+
+--13. obtenha o nome dos jogadores que tiveram contrato como 'palmeiras'.
+select nome
+from jogador
+where codjogador in
+	(select codjogador
+	from joga
+	where codclube in
+		(select codclube
+		from clube
+		where nome = 'Internacional'));
+
+
+ VAI CAIR NA PROVA ==== VAI CAIR NA PROVA === VAI CAIR NA PROVA === VAI CAIR NA PROVA
+ 
+
+		
+--14. obtenha o nome dos jogadores que NUNCA tiveram contrato com o 'Internacional'.
+select nome
+from jogador
+where codjogador not in
+	(select codjogador
+	from joga
+	where codclube in
+		(select codclube
+		from clube
+		where nome = 'Internacional'));
+
